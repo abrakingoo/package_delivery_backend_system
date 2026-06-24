fb869707393a4eed856d4581679e7d4a2e29cb39
abrakingoo <maikoabraham68@gmail.com>
2026-06-24 11:23:36 +0300

    feat: add handling for missing parameters in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,10 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid do |e|
         render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
     end
+
+    rescue_from ActionController::ParameterMissing do |e|
+        render json: {
+        error: e.message
+        }, status: :bad_request
+    end
 end
