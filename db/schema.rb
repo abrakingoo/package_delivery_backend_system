a8e33a8df92e0a92890c77146a39567cbf416df9
abrakingoo <maikoabraham68@gmail.com>
2026-06-24 14:41:21 +0300

    feat: add authentication fields to Driver model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_24_100903) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_24_105519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,9 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_24_100903) do
     t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
+    t.index ["email"], name: "index_drivers_on_email", unique: true
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
