4a9a0e42d711012ebce2176b259f376bf36c9463
abrakingoo <maikoabraham68@gmail.com>
2026-06-24 18:28:22 +0300

    feat: implement NearestDriverAssignmentService for assigning nearby available drivers to delivery requests

---
+++ app/services/nearest_driver_assignment_service.rb
@@ -0,0 +1,15 @@
+class NearestDriverAssignmentService
+    def self.call(delivery_request, location)
+        nearby_driver_ids = DriverLocation.near(
+            [location[:latitude], location[:longitude]], 10, order: false
+        ).pluck(:driver_id)
+
+        drivers = Driver.where(available: true, id: nearby_driver_ids).limit(10)
+
+        drivers.each do |driver|
+            DriverRequest.create!(delivery_request: delivery_request, driver: driver)
+        end
+
+        drivers
+    end
+end
