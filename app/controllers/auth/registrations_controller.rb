7d071360f78fbdc007a396cc4db841b2a1ecd1cf
abrakingoo <maikoabraham68@gmail.com>
2026-06-24 09:01:04 +0300

    feat: add registrations controller for user sign-up

---
+++ app/controllers/auth/registrations_controller.rb
@@ -0,0 +1,25 @@
+class Auth::RegistrationsController < ApplicationController
+    def create
+        if params[:password] != params[:password_confirmation] 
+            render json: {error: "Password and password confirmation don't match"}
+        end
+
+        result = RegistrationService.call(user_params)
+
+        if result[:success]
+            render json: {
+            message: result[:message],
+            user: result[:user]
+            }, status: :created
+        else
+            render json: {
+            error: result[:message]
+            }, status: :conflict
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
