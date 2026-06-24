5f51cc68cdda728544cc520a53477ed44927e846
abrakingoo <maikoabraham68@gmail.com>
2026-06-24 12:41:44 +0300

    feat: update DeliveryRequestService to include user ID in request creation and improve address geocoding

--- app/services/delivery_request_service.rb
+++ app/services/delivery_request_service.rb
@@ -1,27 +1,35 @@
 class DeliveryRequestService
-    def self.call(params)
+    def self.call(params, user_id)
 
-        pickup = GeocodingService.coordinates_for(params[:pick_up_address])
-        return {
-        success: false,
-        error: "Unable to geocode pickup address"
-        } unless pickup
+        pickup_addr = params[:pick_up_address]
+        pickup_address = "#{pickup_addr[:street]}, #{pickup_addr[:city]}, #{pickup_addr[:country]}"
+        pickup = GeocodingService.coordinates_for(pickup_address)
+        return { success: false, error: "Unable to geocode pickup address" } unless pickup
 
+        delivery_addr = params[:delivery_address]
+        delivery_address = "#{delivery_addr[:street]}, #{delivery_addr[:city]}, #{delivery_addr[:country]}"
+        delivery = GeocodingService.coordinates_for(delivery_address)
+        return { success: false, error: "Unable to geocode delivery address" } unless delivery
 
-        delivery = GeocodingService.coordinates_for(params[:delivery_address])
-        return {
-        success: false,
-        error: "Unable to geocode delivery address"
-        } unless delivery
-        
-        Rails.logger.debug pickup.inspect
-        Rails.logger.debug delivery.inspect
-        params.merge!(
+        request = DeliveryRequest.create!(
+            user_id: user_id,
+            status: "requested",
+            package_description: params[:description],
+            weight: params[:weight]
+        )
+
+        Address.create!(
+            delivery_request: request,
+            pickup_street: pickup_addr[:street],
+            pickup_city: pickup_addr[:city],
             pickup_latitude: pickup[:latitude],
             pickup_longitude: pickup[:longitude],
+            delivery_street: delivery_addr[:street],
+            delivery_city: delivery_addr[:city],
             delivery_latitude: delivery[:latitude],
             delivery_longitude: delivery[:longitude]
         )
-        return params
+
+        return { success: true, request: request }
     end
 end
