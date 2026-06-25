3ffd4b250ab1d4eeda4df1a337dd05589c3b7fcf
abrakingoo <maikoabraham68@gmail.com>
2026-06-25 09:07:28 +0300

    refactor: remove unused 'available' parameter from Driver creation

--- app/services/registration_service.rb
+++ app/services/registration_service.rb
@@ -3,7 +3,7 @@ class RegistrationService
     case role.to_s.downcase
     when "driver"
       return { success: false, message: "Driver already exists" } if Driver.exists?(email: params[:email])
-      actor = Driver.create!(params.slice(:name, :email, :phone, :available, :password, :password_confirmation))
+      actor = Driver.create!(params.slice(:name, :email, :phone, :password, :password_confirmation))
     else
       return { success: false, message: "User already exists" } if User.exists?(email: params[:email])
       actor = User.create!(params.slice(:name, :email, :password, :password_confirmation))
